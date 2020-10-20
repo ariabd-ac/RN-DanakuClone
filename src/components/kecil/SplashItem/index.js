873392f6d755f434414fbc7b432d339c69b7291a
rizalazky <rizalaazky26@gmail.com>
2020-10-20 16:27:55 +0700

    update splash

--- src/components/kecil/SplashItem/index.js
+++ src/components/kecil/SplashItem/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, View,Image } from 'react-native'
+import { SplashIcon1 } from '../../../assets/assets/SPLASH'
+import { Colors } from '../../../utils'
 
 const SplashItemWidth=Dimensions.get('window').width
 
-const SplashItem = ({i}) => {
+const SplashItem = ({title,desc,imageSource}) => {
 
     return (
         <View style={styles.container}>
-            <Text>Splash {i}</Text>
+            <View style={styles.image_container}>
+                <Image source={imageSource} style={styles.image} resizeMode='contain'/>
+            </View>
+            <View style={styles.text_container}>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.desc}>{desc}</Text>
+            </View>
         </View>
     )
 }
@@ -17,10 +25,31 @@ export default SplashItem
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor:'yellow',
+        backgroundColor:Colors.background,
         width:SplashItemWidth,
         justifyContent:"center",
         alignItems:"center",
-        margin:1
+    },
+    image_container:{
+        flex:1,
+        overflow:'hidden',
+    },
+    image:{
+        width:410,
+        height:600
+    },
+    text_container:{
+        justifyContent:"center",
+        alignItems:"center",
+    },
+    title:{
+        color:Colors.text,
+        fontSize:30,
+        textAlign:"center"
+    },
+    desc:{
+        color:Colors.text,
+        fontSize:20,
+        textAlign:"center"
     }
 })
--- src/pages/EnterNumber/index.js
+++ src/pages/EnterNumber/index.js
@@ -1,28 +1,29 @@
 import React from 'react'
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler'
+import { Image, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { DanainText } from '../../assets/assets/SPLASH'
+import { DanainText, SplashIcon1,SplashIcon2,SplashIcon3 } from '../../assets/assets/SPLASH'
 import { IconApp } from '../../assets/assets/top-icon'
 import { SplashItem } from '../../components'
+import { Colors } from '../../utils'
 
 const EnterNumber = () => {
     return (
         <View style={styles.page}>
             <View style={styles.logo_wrapper}>
-                {/* <Image source={<IconApp/>} height={50} width={50}/>
-                <Image source={<DanainText/>} height={50} width={60}/> */}
+                <Image source={IconApp} style={styles.logo} resizeMode='contain'/>
+                <Image source={DanainText} style={styles.danainText} resizeMode='contain'/>
             </View>
             <SafeAreaView style={styles.scrool_wrapper}>
                 <ScrollView  horizontal
                     pagingEnabled
                     showsHorizontalScrollIndicator={false}
                 >
-                    <SplashItem i={1}/>
-                    <SplashItem i={2}/>
-                    <SplashItem i={3}/>
+                    <SplashItem title='The digital wallset for you!' desc='Keep your cash as well as debit/credit cards seanlessly in DANA' imageSource={SplashIcon1}/>
+                    <SplashItem title='Payments are easier than ever!' desc='Shopping at merchants?Use scan QRIS.Online Shopping?Use send money' imageSource={SplashIcon2}/>
+                    <SplashItem title='Stay safe with DANA!' desc='DANA Protection secures your money and data,complete with PIN code for a better security' imageSource={SplashIcon3}/>
                 </ScrollView>
             </SafeAreaView>
+            <StatusBar/>
             <Text>Enter your mobile number to continue</Text>
             {/* <TextInput/> */}
         </View>
@@ -33,11 +34,25 @@ export default EnterNumber
 
 const styles = StyleSheet.create({
     logo_wrapper:{
-        flexDirection:"row"
+        flexDirection:"row",
+        backgroundColor:Colors.header,
+        justifyContent:"center",
+        paddingVertical:15,
+        alignItems:"center"
+    },
+    logo:{
+        height:30,
+        width:30,
+        marginRight:10
+    },
+    danainText:{
+        height:15,
+        width:100,
     },
     page:{
         flex:1,
-        justifyContent:"space-around"
+        justifyContent:"space-around",
+        backgroundColor:Colors.background
     },
     scrool_wrapper:{
         flex:1,
